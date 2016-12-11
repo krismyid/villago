838c269f28a9933a361c8734d762d3feaef0e112
Christian Windi Utomo <utomo@codigo.id>
2016-12-11 19:10:47 +0700

    simplified index

---
+++ footer.inc.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+  <div class="container">
+      <nav class="pull-left">
+          <ul>
+      <li>
+        <a href="#">
+          Kebijakan Layanan
+        </a>
+      </li>
+      <li>
+        <a href="#">
+           Kebijakan Privasi
+        </a>
+      </li>
+      <li>
+        <a href="#">
+           Hubungi Kami
+        </a>
+      </li>
+
+          </ul>
+      </nav>
+      <div class="copyright pull-right">
+          &copy; 2016, made with <i class="material-icons">favorite</i> from Jakarta, Indonesia - VillaGo.
+      </div>
+  </div>
+</footer>
